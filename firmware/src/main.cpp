82e487245437fd5888775765d3fd4d628fdc2407
DutchMaker <ruud@partech.nl>
2023-10-27 07:46:43 +0200

    Working out flickering issue

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -24,7 +24,7 @@
 IntervalTimer ips_hsyncTimer;
 IntervalTimer ips_vsyncTimer;
 
-uint8_t frameBuffer[160][144];
+volatile uint8_t frameBuffer[160][144];
 volatile unsigned int ips_currentLine = 0;
 volatile unsigned int ips_currentPixel = 0;
 
@@ -37,6 +37,8 @@ int sv_currentLine = 0;
 bool sv_skip_line = false;
 int sv_currentPixel = 0;
 bool ips_rendering_frame = false;
+
+int ips_current_frame = 0;
 
 void draw_test_screen();
 void render_ips_frame();
@@ -97,6 +99,7 @@ void draw_test_screen() {
     }
   }
 
+  
   // Draw a circle
   int centerX = 80;
   int centerY = 72;
@@ -104,11 +107,39 @@ void draw_test_screen() {
   for (int i = 0; i < 160; i++) {
       for (int j = 0; j < 144; j++) {
           if (sqrt(pow(i - centerX, 2) + pow(j - centerY, 2)) <= radius) {
-              frameBuffer[i][j] = HIGH;
-              frameBuffer[i][j] = HIGH;
+              frameBuffer[i][j] = 0;
+
+              if (j % 2 == 0) {
+                frameBuffer[i][j] = 1;
+              }
+              else if (j % 3 == 0) {
+                frameBuffer[i][j] = 2;
+              }
+              else if (j % 4 == 0) {
+                frameBuffer[i][j] = 3;
+              }
           }
       }
   }
+
+  for (int i = 0; i < 160; i++) {
+    frameBuffer[i][0] = 0;
+    frameBuffer[i][1] = 0;
+    frameBuffer[i][3] = 0;
+
+    frameBuffer[i][4] = 1;
+    frameBuffer[i][5] = 1;
+    frameBuffer[i][6] = 1;
+
+    frameBuffer[i][7] = 2;
+    frameBuffer[i][8] = 2;
+    frameBuffer[i][9] = 2;
+
+    frameBuffer[i][10] = 3;
+    frameBuffer[i][11] = 3;
+    frameBuffer[i][12] = 3;
+  }
+
 }
 
 //////////////////////////////////////////////////////////////////////////
@@ -123,6 +154,8 @@ void render_ips_frame() {
   digitalWriteFast(PIN_IPS_DATA1, (frameBuffer[ips_currentPixel][ips_currentLine] >> 1) & 1);
   delayNanoseconds(5);
   digitalWriteFast(PIN_IPS_CLOCK, LOW);
+  delayNanoseconds(5);
+  
   ips_currentPixel++;
 }
 
@@ -149,7 +182,7 @@ void capture_sv_frame() {
 
   if (sv_pixel_clock == HIGH && sv_pin_state_clock != sv_pixel_clock) {
     // Pixel clock transitioned from low to high.
-    if (sv_currentPixel < 160 - 3 && !sv_skip_line) {
+    if (sv_currentPixel < 157 && !sv_skip_line && sv_currentLine < 144) {
       if (sv_currentField == 0) {
         frameBuffer[sv_currentPixel++][sv_currentLine] = digitalReadFast(PIN_SV_DATA0);
         frameBuffer[sv_currentPixel++][sv_currentLine] = digitalReadFast(PIN_SV_DATA1);
@@ -169,28 +202,19 @@ void capture_sv_frame() {
     if (sv_currentLine % 10 == 0 && !sv_skip_line) {  
       // Skip every 10th line. SuperVision has 160 lines, the IPS has 144.
       sv_skip_line = true;
-      return;
-    }
-
-    sv_skip_line = false;
-    
-    if (sv_currentLine < 159) {
-      sv_currentLine++;
-      sv_currentPixel = 0;
     }
     else {
+      sv_skip_line = false;    
+      sv_currentLine++;
       sv_currentPixel = 0;
     }
   }
 
   if (sv_frame_polarity == HIGH && sv_pin_state_frame_polarity != sv_frame_polarity) {
     // Frame polarity transitioned from low to high.
-    sv_currentField = 0;
-    sv_currentLine = 0;
-    sv_currentPixel = 0;
-
     // Start the timers to render the IPS screen.
     ips_rendering_frame = true;
+
     ips_hsyncTimer.begin(ips_hsync, 108);
     ips_vsyncTimer.begin(ips_vsync, 16666);
 
@@ -207,14 +231,18 @@ void capture_sv_frame() {
   sv_pin_state_line_latch = sv_line_latch;
   sv_pin_state_frame_polarity = sv_frame_polarity;
 
-  digitalWriteFast(PIN_TEST_SV_PIXEL_CLOCK, sv_pixel_clock);
-  digitalWriteFast(PIN_TEST_SV_LINE_LATCH, sv_line_latch);
-  digitalWriteFast(PIN_TEST_SV_FRAME_POLARITY, sv_frame_polarity);
+  // digitalWriteFast(PIN_TEST_SV_PIXEL_CLOCK, sv_pixel_clock);
+  // digitalWriteFast(PIN_TEST_SV_LINE_LATCH, sv_line_latch);
+  // digitalWriteFast(PIN_TEST_SV_FRAME_POLARITY, sv_frame_polarity);
 }
 
 //////////////////////////////////////////////////////////////////////////
 void ips_hsync() {
   if (ips_currentLine == 144) {
+    return;
+  }
+
+  if (ips_current_frame % 25 != 0) {
     return;
   }
 
@@ -228,12 +256,18 @@ void ips_hsync() {
 
 //////////////////////////////////////////////////////////////////////////
 void ips_vsync() {
-  digitalWriteFast(PIN_IPS_VSYNC, HIGH);
-  delayNanoseconds(300);
-  digitalWriteFast(PIN_IPS_VSYNC, LOW);
+  if (++ips_current_frame % 25 != 0) {
+    return;
+  }
+
+  ips_current_frame = 0;
 
   ips_hsyncTimer.end();
   ips_vsyncTimer.end();
+
+  digitalWriteFast(PIN_IPS_VSYNC, HIGH);
+  delayNanoseconds(300);
+  digitalWriteFast(PIN_IPS_VSYNC, LOW);
 
   reset_state();
 }
